4c65f652f23de26bae39d59211d6c68cb3ff25e8
Garvit Sadhwani <garvit.sadh@gmail.com>
2024-03-06 01:08:09 +0530

    slight modifications

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,8 @@
 import {AiOutlineExport} from 'react-icons/ai';
 export default function Footer(){
     return (<div className="footer">
-        Made by <a target="_blank" href="https://garvitsadhwani.github.io">Garvit Sadhwani <AiOutlineExport style={{position:'relative',top:'2px'}}/></a>
+        <div>
+            Made by <a target="_blank" rel="noreferrer" href="https://garvitsadhwani.github.io"> Garvit Sadhwani <AiOutlineExport style={{position:'relative',top:'2px'}}/></a>
+        </div>
     </div>);
 }
